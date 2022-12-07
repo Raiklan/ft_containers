7d5d4359cdfb55e0e3fa38e9a81afe23354205ed
Raiklan <sami.aich@gmail.com>
2022-12-07 19:17:31 +0100

    Random access iterator implemented and tested

---
+++ RAiterator.hpp
@@ -0,0 +1,156 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RAiterator.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saich <saich@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/01 19:16:15 by saich             #+#    #+#             */
+/*   Updated: 2022/12/07 19:11:38 by saich            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RAITERATOR_HPP
+# define RAITERATOR_HPP
+
+# include "iterator.hpp"
+namespace ft
+{
+	template <class T>
+	class random_access_iterator
+	{
+		public:
+			typedef typename ft::iterator<std::random_access_iterator_tag, T>::value_type           value_type;
+            typedef typename ft::iterator<std::random_access_iterator_tag, T>::difference_type      difference_type;
+            typedef typename ft::iterator<std::random_access_iterator_tag, T>::pointer              pointer;
+            typedef typename ft::iterator<std::random_access_iterator_tag, T>::reference            reference;
+            typedef typename ft::iterator<std::random_access_iterator_tag, T>::iterator_category    iterator_category;
+
+            random_access_iterator() : my_pointer(0){}
+            random_access_iterator(T* ptr) : my_pointer(ptr){}
+            random_access_iterator(random_access_iterator const & t) : my_pointer(t.base()){}
+
+            pointer base() const {return this->my_pointer;}
+
+            virtual ~random_access_iterator(){}
+
+            reference operator*() { return *my_pointer;};
+
+            /*--------------------ASSIGNMENT/----------------------*/
+
+            random_access_iterator &operator=(random_access_iterator const & t)
+            {
+                this->my_pointer = t.base();
+                return (*this);
+            }
+            random_access_iterator &operator+=(difference_type  t)
+            {
+                this->my_pointer += t;
+                return (*this);
+            }
+            random_access_iterator &operator-=(difference_type  t)
+            {
+                this->my_pointer -= t;
+                return (*this);
+            }
+
+
+            /*--------------------INCREMENTATION/DECREMENTATION----------------------*/
+            
+            random_access_iterator operator++()
+            {
+                this->my_pointer++;
+                return (*this);
+            }
+            random_access_iterator operator--()
+            {
+                this->my_pointer--;
+                return (*this);
+            }
+            random_access_iterator operator++(int) 
+            {
+                random_access_iterator tmp(*this);
+                this->my_pointer++;
+                return (tmp);
+            }
+            random_access_iterator operator--(int)
+            {
+                random_access_iterator tmp(*this);
+                this->my_pointer--;
+                return (tmp);
+            }
+            
+            /*--------------------MATH_OPTIONS----------------------*/
+            random_access_iterator operator+(difference_type n) const
+            {
+                random_access_iterator tmp(this->my_pointer + n);
+                return (tmp);
+            }
+            friend random_access_iterator operator+(difference_type left,const random_access_iterator &r)
+            {
+                return (random_access_iterator(left + r.my_pointer));
+            }
+            random_access_iterator operator-(difference_type n) const
+            {
+                random_access_iterator tmp(this->my_pointer - n);
+                return (tmp);
+            }
+            template <typename U>
+            bool operator==(const random_access_iterator<U> & second)
+            {
+                return (this->my_pointer == second.base());
+            }
+            template <typename U>
+            bool operator!=(const random_access_iterator<U> & second)
+            {
+                return (this->my_pointer != second.base());
+            }
+            template <typename U>
+            bool operator>(const random_access_iterator<U> & second)
+            {
+                return (this->my_pointer > second.base());
+            }
+            template <typename U>
+            bool operator>=(const random_access_iterator<U> & second)
+            {
+                return (this->my_pointer >= second.base());
+            }
+            template <typename U>
+            bool operator<(const random_access_iterator<U> & second)
+            {
+                return (this->my_pointer < second.base());
+            }
+            template <typename U>
+            bool operator<=(const random_access_iterator<U> & second)
+            {
+                return (this->my_pointer <= second.base());
+            }
+            friend random_access_iterator operator-(difference_type left,const random_access_iterator &r)
+            {
+                // random_access_iterator tmp(this->my_pointer + n);
+                return (random_access_iterator(left - r.my_pointer));
+            }
+            difference_type operator-(const random_access_iterator &rhs) const
+            {
+                return (this->base() - rhs.base());
+            }
+            T* operator *() const{
+                return *my_pointer;
+            } 
+            T* operator ->() const {
+                return &(*my_pointer);
+            }
+            T & operator [](difference_type rhs) const {
+                return my_pointer[rhs];
+                }
+            operator random_access_iterator<const T>() const
+            {
+                return random_access_iterator<const T>(my_pointer);
+            }
+            
+
+        protected:
+			T*	my_pointer;
+    };
+}
+#endif
